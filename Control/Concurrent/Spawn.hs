a33eb2cb945cd886f59d080cfa24e8f051653562
Keegan McAllister <mcallister.keegan@gmail.com>
2010-09-21 09:18:50 -0400

    initial commit

---
+++ Control/Concurrent/Spawn.hs
@@ -0,0 +1,32 @@
+module Control.Concurrent.Spawn(
+    -- * Spawn 
+    spawn
+
+    -- * Spawn with @'try'@
+  , Result
+  , spawnTry) where
+
+import Control.Concurrent
+import Control.Exception
+
+-- | Two ways a computation of type @'IO' a@ can end.
+type Result a = Either SomeException a
+
+-- | Spawn a concurrent computation.  Produces an action which
+-- demands a @'Result'@.
+spawnTry :: IO a -> IO (IO (Result a))
+spawnTry m = do
+  v <- newEmptyMVar
+  -- block async exns, then unblock inside 'try' unless parent was blocked
+  -- avoids dropping an exception
+  b <- blocked
+  _ <- block $ forkIO (try (if b then m else unblock m) >>= putMVar v)
+  return (readMVar v)
+
+-- | Spawn a concurrent computation.  Produces an action which
+-- demands the result.  Any exception from the original computation
+-- is re-thrown when and where the result is demanded.
+spawn :: IO a -> IO (IO a)
+spawn m = do
+  r <- spawnTry m
+  return (r >>= either throwIO return)
